4338618a933745cb5c1135b34d1b0b21105600da
Ruinwalker7 <973521624@qq.com>
2023-11-24 16:06:16 +0800

    first commit

---
+++ QR-app/settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+//        maven { setUrl("https://maven.aliyun.com/repository/google") }
+//        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+//        maven { setUrl("https://maven.aliyun.com/repository/google") }
+//        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
