770b63ddb6011d6432147ec530188ceee1d256d1
Ruinwalker7 <973521624@qq.com>
2023-11-30 16:53:26 +0800

    能够访问到jsp网站

--- qrCode-web/src/main/java/com/chen/qrcode/controller/webController.java
+++ qrCode-web/src/main/java/com/chen/qrcode/controller/webController.java
@@ -1,13 +1,22 @@
 package com.chen.qrcode.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class webController {
     @RequestMapping("/")
     String home(){
         return "Hello World";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("msg","跟一一哥学习SpringBoot中使用JSP功能!");
+        //要跳转到的页面视图名称
+        return "index";
     }
 }
---
+++ qrCode-web/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE HTML>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Boot支持JSP!</title>
+    <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+<h2>Hello ${msg}</h2>
+</body>
+</html>
