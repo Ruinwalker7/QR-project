8d00b84e1c2e281e3c5b97611cbe2939439cbf1c
Ruinwalker7 <973521624@qq.com>
2024-02-05 17:13:08 +0800

    用户

--- QR-Delivery-Web/src/main/java/com/chen/qrcode/controller/CustomerController.java
+++ QR-Delivery-Web/src/main/java/com/chen/qrcode/controller/CustomerController.java
@@ -26,20 +26,15 @@ public class CustomerController {
 
     @Autowired
     private ObjectMapper objectMapper; // 使用Jackson ObjectMapper将对象转换为JSON
+
     @GetMapping("/all")
-    public String getAllDelivery( @RequestParam(name="page", defaultValue = "1") Integer pageNums,
-                                  @RequestParam(defaultValue = "10") Integer limit,
-                                  @RequestParam(name="username",required = false) String id,
-                                  @RequestParam(name="phone",required = false) String phone
-                                 ) {
-        IPage<CustomerEntity> iPage =  customerService.findAll(pageNums,limit,id,phone);
+    public String getAllDelivery(@RequestParam(name = "page", defaultValue = "1") Integer pageNums, @RequestParam(defaultValue = "10") Integer limit, @RequestParam(name = "username", required = false) String id, @RequestParam(name = "phone", required = false) String phone) {
+        IPage<CustomerEntity> iPage = customerService.findAll(pageNums, limit, id, phone);
         List<CustomerEntity> deliveryEntities = iPage.getRecords();
         System.out.println(deliveryEntities);
 
         try {
-            return "{\"code\": 0,\"msg\": \"\",\n" +
-                    "    \"count\": "+ iPage.getTotal()+",\n" +
-                    "    \"data\": " + objectMapper.writeValueAsString(deliveryEntities) +"}";
+            return "{\"code\": 0,\"msg\": \"\",\n" + "    \"count\": " + iPage.getTotal() + ",\n" + "    \"data\": " + objectMapper.writeValueAsString(deliveryEntities) + "}";
         } catch (JsonProcessingException e) {
             e.printStackTrace();
             return "Error converting to JSON";
@@ -47,11 +42,11 @@ public class CustomerController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<String> delete(@RequestParam Long id){
+    public ResponseEntity<String> delete(@RequestParam Long id) {
         System.out.println(id);
         int rowsAffected = customerDao.deleteById(id);
         if (rowsAffected > 0) {
-            return  ResponseEntity.status(HttpStatus.OK).body("删除成功");
+            return ResponseEntity.status(HttpStatus.OK).body("删除成功");
         } else {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("删除失败");
         }
